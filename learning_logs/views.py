82ea4dd14cf98d01b291d3015e08280ef6aa1d90
Kacper Beirowski <kacper.beirowski@gmail.com>
2024-07-25 16:50:26 +0200

    ADD: Topic and Entries pages

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,5 +1,21 @@
 from django.shortcuts import render
 
+from learning_logs.models import Topic
+
+
 def index(request):
     """Home page for Learning_log app"""
     return render(request, 'learning_logs/index.html')
+
+def topics(request):
+    """Display all topics"""
+    topics = Topic.objects.order_by('date_added')
+    context = {'topics': topics}
+    return render(request, 'learning_logs/topics.html', context)
+
+def topic(requset, topic_id):
+    """Display single topic with entries"""
+    topic = Topic.objects.get(id=topic_id)
+    entries = topic.entry_set.order_by('-date_added')
+    context = {'topic': topic, 'entries': entries}
+    return render(requset, 'learning_logs/topic.html', context)
